dff855171e4dd9cdcfcf88b61ec586ad4c218e0f
Desire KOUASSI <kouamdesire90@gmail.com>
2025-06-22 15:29:20 +0000

    feat: Create Dashboard
    - Add sidebar and items
    
    Fixes #3

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,7 +19,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       </Link>
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="flex items-center self-center gap-2 font-medium">
-          <AppLogo />
+          <AppLogo iconClassName="size-8" />
         </Link>
         {children}
         <div className="text-balance text-center text-xs text-muted-foreground">
